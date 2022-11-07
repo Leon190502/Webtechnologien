77650f3c5d7e4984511b835fb8e2a60df688ff3c
leon.sas <Yoda2002>
2022-11-07 18:22:08 +0100

    commit of 1.17

--- ReactExcercises/exercise-1.6.-1.11/src/App.js
+++ ReactExcercises/exercise-1.6.-1.11/src/App.js
@@ -7,6 +7,9 @@ const Statistics = (props) => {
       <p>Good {props.good}</p>
       <p>Neutral {props.neutral}</p>
       <p>Bad {props.bad}</p>
+      <p>Sum {props.sum}</p>
+      <p>Positive {props.posPer}%</p>
+      <p>Average {props.aver}</p>
     </div>
    )
 }
@@ -28,17 +31,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const sum = good + neutral + bad
+  const [average, setAverage] = useState(0)
+  const aver = average/sum
+  const posPer = good/sum*100
 
-  const goodClick = () => setGood(good + 1)
-  const neutralClick = () => setNeutral(neutral + 1)
-  const badClick = () => setBad(bad + 1)
+  const goodClick = () => {
+    setGood(good + 1)
+    setAverage(average + 1)
+  }
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+    setAverage(average + 0)
+  }
+  const badClick = () => {
+    setBad(bad + 1)
+    setAverage(average - 1)
+  }
 
   return (
     <div>
       <h1>Feedback</h1>
       <Feedback button1={goodClick} button2={neutralClick} button3={badClick}/>
       <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} sum={sum} posPer={posPer} aver={aver}/>
     </div>
   )
 }
