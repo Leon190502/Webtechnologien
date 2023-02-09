2d5f0804e7c11a86a2f850be808c1367e2c2d560
leon.sas <Yoda2002>
2023-02-09 10:23:07 +0100

    1.9

--- Fullstack Open/unicafe/src/App.js
+++ Fullstack Open/unicafe/src/App.js
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = (props) => {
+const StaticticsLine = (props) => {
+	return (
+		<p>{props.text} {props.value}</p>
+	)
+}
 
+const Statistics = (props) => {
 	if (props.all > 0) {
 		return (
-
 			<div><h1>Statistics</h1>
-				<p>Good {props.good}</p>
-				<p>Neutral {props.neutral}</p>
-				<p>Bad {props.bad}</p>
-				<p>All {props.all}</p>
-				<p>Average {props.average}</p>
-				<p>Positive {props.positive} %</p>
+				<StaticticsLine text={"Good"} value={props.good} />
+				<StaticticsLine text={"Neutral"} value={props.neutral} />
+				<StaticticsLine text={"Bad"} value={props.bad} />
+				<StaticticsLine text={"All"} value={props.all} />
+				<StaticticsLine text={"Average"} value={props.average} />
+				<StaticticsLine text={"Positive"} value={props.positive} />
 			</div>
 		)
 	} else {
