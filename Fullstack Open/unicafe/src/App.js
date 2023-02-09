80fd553907e3e61491031daa900fedd068549722
leon.sas <Yoda2002>
2023-02-09 02:07:45 +0100

    1.8

--- Fullstack Open/unicafe/src/App.js
+++ Fullstack Open/unicafe/src/App.js
@@ -1,6 +1,19 @@
 import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+const Statistics = (props) => {
+	return (
+		<div><h1>Statistics</h1>
+			<p>Good {props.good}</p>
+			<p>Neutral {props.neutral}</p>
+			<p>Bad {props.bad}</p>
+			<p>All {props.all}</p>
+			<p>Average {props.average}</p>
+			<p>Positive {props.positive} %</p>
+		</div>
+	)
+}
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0)
@@ -17,13 +30,8 @@ const App = () => {
 			<Button onClick={increment(good, setGood)} text={"good"} />
 			<Button onClick={increment(neutral, setNeutral)} text={"neutral"} />
 			<Button onClick={increment(bad, setBad)} text={"bad"} />
-			<h1>statistics</h1>
-			<p>Good {good}</p>
-			<p>Neutral {neutral}</p>
-			<p>Bad {bad}</p>
-			<p>All {all}</p>
-			<p>Average {(good - bad) / all}</p>
-			<p>Positive {good / all * 100} %</p>
+			<Statistics good={good} neutral={neutral} bad={bad} all={all} average={(good - bad) / all} positive={good / all * 100} />
+
 		</div>
 	)
 }
