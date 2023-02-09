99ccc1672e9ce4a3c24dfdfb47231118959d9821
leon.sas <Yoda2002>
2023-02-09 11:31:27 +0100

    2.9

--- Fullstack Open/phonebook/src/App.js
+++ Fullstack Open/phonebook/src/App.js
@@ -2,16 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{
-			name: 'Arto Hellas',
-			number: 12342123
-		}
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
 	])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
+	const [search, setSearch] = useState('')
 
 	const handleNewNameChange = e => setNewName(e.target.value)
 	const handleNewNumberChange = e => setNewNumber(e.target.value)
+	const handleSearch = e => setSearch(e.target.value)
 
 	const handleAddNewPerson = e => {
 		e.preventDefault()
@@ -32,6 +34,9 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				search: <input value={search} onChange={handleSearch} />
+			</div>
 			<form onSubmit={handleAddNewPerson}>
 				<div>
 					name: <input value={newName} onChange={handleNewNameChange} />
@@ -44,7 +49,7 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+			{persons.filter(person => person.name.toLocaleLowerCase().includes(search)).map(person => <p key={person.id}>{person.name} {person.number}</p>)}
 		</div>
 	)
 }
