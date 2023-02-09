9911d8c58dddaf0cb87bc7c2d48f67a76597f3cf
leon.sas <Yoda2002>
2023-02-09 11:24:12 +0100

    2.8

--- Fullstack Open/phonebook/src/App.js
+++ Fullstack Open/phonebook/src/App.js
@@ -2,11 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas' }
+		{
+			name: 'Arto Hellas',
+			number: 12342123
+		}
 	])
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	const handleNewNameChange = e => setNewName(e.target.value)
+	const handleNewNumberChange = e => setNewNumber(e.target.value)
 
 	const handleAddNewPerson = e => {
 		e.preventDefault()
@@ -14,11 +19,13 @@ const App = () => {
 		if (persons.find(person => person.name === newName)) {
 			alert(`${newName} is already in the Phonebook`)
 			setNewName('')
+			setNewNumber('')
 
 		} else {
-			const newPerson = { name: newName }
+			const newPerson = { name: newName, number: newNumber }
 			setPersons(persons.concat(newPerson))
 			setNewName('')
+			setNewNumber('')
 		}
 	}
 
@@ -30,11 +37,14 @@ const App = () => {
 					name: <input value={newName} onChange={handleNewNameChange} />
 				</div>
 				<div>
+					number: <input value={newNumber} onChange={handleNewNumberChange} />
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map(person => <p key={person.name}>{person.name}</p>)}
+			{persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
 		</div>
 	)
 }
