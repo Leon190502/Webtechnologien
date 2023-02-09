b6be8afdeb8547557d8ff66e31a01dc24d5915c3
leon.sas <Yoda2002>
2023-02-09 10:19:05 +0100

    1.9

--- Fullstack Open/unicafe/src/App.js
+++ Fullstack Open/unicafe/src/App.js
@@ -2,16 +2,24 @@ import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Statistics = (props) => {
-	return (
-		<div><h1>Statistics</h1>
-			<p>Good {props.good}</p>
-			<p>Neutral {props.neutral}</p>
-			<p>Bad {props.bad}</p>
-			<p>All {props.all}</p>
-			<p>Average {props.average}</p>
-			<p>Positive {props.positive} %</p>
-		</div>
-	)
+
+	if (props.all > 0) {
+		return (
+
+			<div><h1>Statistics</h1>
+				<p>Good {props.good}</p>
+				<p>Neutral {props.neutral}</p>
+				<p>Bad {props.bad}</p>
+				<p>All {props.all}</p>
+				<p>Average {props.average}</p>
+				<p>Positive {props.positive} %</p>
+			</div>
+		)
+	} else {
+		return (
+			<div><p>No feedback given</p></div>
+		)
+	}
 }
 
 const App = () => {
