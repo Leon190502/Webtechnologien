951b98cf83372fa8b59accdc0a77835f005d30a8
leon.sas <Yoda2002>
2023-02-21 12:59:10 +0100

    3.22

--- Fullstack Open/phonebook/backend/index.js
+++ Fullstack Open/phonebook/backend/index.js
@@ -3,13 +3,13 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
+  console.error(error.message)
 
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	}
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-	next(error)
+  next(error)
 }
 
 const app = express()
@@ -22,65 +22,65 @@ app.use(errorHandler)
 //________________________________Getter_____________________________
 //Get all
 app.get('/api/persons', (request, response) => {
-	Person.find({}).then(persons => {
-		response.json(persons)
-	})
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	Person.findById(request.params.id)
-		.then(person => {
-			if (person) {
-				response.json(person)
-			} else {
-				response.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 //______________________________Delete_________________________________
 //Delete single person
 app.delete('/api/persons/:id', (request, response, next) => {
-	console.log('deleting peron')
-	Person.findByIdAndRemove(request.params.id)
-		.then(result => {
-			response.status(204).end()
-		})
-		.catch(error => next(error))
+  console.log('deleting peron')
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //______________________________Post_____________________________________
 //Post a single person
 app.post('/api/persons', (request, response) => {
-	if (!request.body.name) {
-		response.status(400).json({
-			error: 'name  is missing',
-		})
-	}
-	if (!request.body.number) {
-		response.status(400).json({
-			error: 'number  is missing',
-		})
-	}
+  if (!request.body.name) {
+    response.status(400).json({
+      error: 'name  is missing',
+    })
+  }
+  if (!request.body.number) {
+    response.status(400).json({
+      error: 'number  is missing',
+    })
+  }
 
-	const newName = request.body.name
-	const newNumber = request.body.number
-	const newPerson = new Person({
-		name: newName,
-		number: newNumber
-	})
+  const newName = request.body.name
+  const newNumber = request.body.number
+  const newPerson = new Person({
+    name: newName,
+    number: newNumber
+  })
 
-	newPerson.save().then(persons => {
-		response.json(persons)
-	})
+  newPerson.save().then(persons => {
+    response.json(persons)
+  })
 })
 
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
 
 
