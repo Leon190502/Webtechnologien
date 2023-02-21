e86cfaa0ae00e7e37a4c1fabe06902709262ef07
leon.sas <Yoda2002>
2023-02-21 12:37:53 +0100

    3.16

--- Fullstack Open/phonebook/backend/index.js
+++ Fullstack Open/phonebook/backend/index.js
@@ -1,36 +1,22 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Person = require('./models/person')
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(errorHandler)
 
-
-let persons = [
-	{
-		"id": 1,
-		"name": "Arto Hellas",
-		"number": "040-123456"
-	},
-	{
-		"id": 2,
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523"
-	},
-	{
-		"id": 3,
-		"name": "Dan Abramov",
-		"number": "12-43-234345"
-	},
-	{
-		"id": 4,
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122"
-	}
-]
-const generatedID = () => Math.floor((Math.random() * 1000000) + 1)
 
 //________________________________Requests___________________________
 //________________________________Getter_____________________________
@@ -50,10 +36,7 @@ app.get('/api/persons/:id', (request, response) => {
 				response.status(404).end()
 			}
 		})
-		.catch(error => {
-			console.log(error)
-			response.status(400).send({ error: 'malformatted id' })
-		})
+		.catch(error => next(error))
 })
 
 
@@ -95,8 +78,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`)
 })
+
+
+
