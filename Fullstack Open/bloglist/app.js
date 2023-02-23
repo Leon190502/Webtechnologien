bbcf22dad1ed30edb6a131aa768389912c345b73
leon.sas <Yoda2002>
2023-02-23 14:09:04 +0100

    4.15

--- Fullstack Open/bloglist/app.js
+++ Fullstack Open/bloglist/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const { blogsRouter } = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const { usersRouter } = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI);
 
@@ -13,6 +14,8 @@ logger.info("connected to MongoDB");
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
---
+++ Fullstack Open/bloglist/controllers/users.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const { User } = require('../models/user')
+
+const usersRouter = express.Router()
+
+usersRouter.get('/', async (request, response) => {
+	//jwt.verify(request.token, process.env.SECRET)
+	const users = await User.find({}).populate(
+		'blogs', { 'url': 1, 'title': 1, 'author': 1 }
+	)
+	response.json(users.map(u => u.toJSON()))
+})
+
+usersRouter.post('/', async (request, response) => {
+	const body = request.body
+
+	/*if (body.username.length <= 3) {
+		return response.status(400).send({ error: 'username length less than 3' })
+	}
+	if (body.password.length <= 3) {
+		return response.status(400).send({ error: 'password length less than 3' })
+	}*/
+
+	const saltRounds = 10
+	const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+	const user = new User({
+		"username": body.username,
+		"name": body.name,
+		passwordHash,
+	})
+
+	const savedUser = await user.save()
+	response.json(savedUser)
+})
+
+exports.usersRouter = usersRouter;
